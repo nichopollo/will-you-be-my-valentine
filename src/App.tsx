c24abfa096055749c256becbeedc87fe30c3c96e
nichopollo <nichopollo09@gmail.com>
2026-02-04 16:03:13 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,19 +13,19 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
+      "Sicura?",
+      "Se te lo chiedo gentilmente?",
+      "PERFAVOREE",
+      "se ti porto una cioccolata?",
+      "ci facciamo una chiacchierata in mezzo ai fiori?",
+      "Perfavore fragolinaaa",
+      "daii :*(",
+      "rimarrò triste senò",
+      "Si adesso sono triste",
       "ok ur talking to nathan's ghost",
-      "please babe",
+      "eddaaaiii",
       ":((((",
-      "PRETTY PLEASE",
+      "PERPERPERFAVOOOREE?",
       "Estoy muerto",
       "No :(",
     ];
@@ -38,7 +38,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold">Grazieeee ti amo tanto!! ;))</div>
         </>
       ) : (
         <>
@@ -46,7 +46,7 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-4xl">Vuoi essere il mio valentino gino?</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
